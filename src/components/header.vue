90bf9733a864e6b108b52e43de7ca7789be92a70
LupoArticoblu <wolf94-@libero.it>
2024-07-06 12:56:50 +0200

    header[scss]

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,5 +1,20 @@
 <template>
-  <header>Sono header</header>
+  <span>Sono header</span>
+  <header>
+    <div class="logo">
+      <!-- percorso assoluto perchè l'immagine è in public-->
+      <img src="/public/Logo.png" alt="">  
+    </div>
+
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Prodotti</a></li>
+        <li><a href="#">Chi siamo</a></li>
+        <li><a href="#">Contatti</a></li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
@@ -8,6 +23,32 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+//inseriamo la mixin
+@use '../Style/partials/mixin' as *;
+//inseriamo la variabile
+@use '../Style/partials/variabili' as *;
 
+header {
+  text-align: center;
+  img {
+    width: 200px;
+  }
+  ul{
+    list-style: none;
+    @include center('horizontal');
+    a{
+      text-decoration: none;
+      display: inline-block;
+      padding: 1rem;
+      color: black;
+      font-weight: 700;
+      &:hover, 
+      &:focus {
+        // con scss abbiamo anche una peculiarità, con lighten e darken partendo da un colore x possiamo dargli delle percentuali per renderlo più luminoso o meno
+        background-color: lighten($primary-color, 70%);
+      }
+    }
+  }
+}
 </style>
